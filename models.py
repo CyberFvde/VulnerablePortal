4d74b68f67373c5219dfc51c1984af122fdae49a
Tony Fadayomi <119269359+CyberFvde@users.noreply.github.com>
2025-03-09 15:20:10 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
 from app import db
 from flask_login import UserMixin
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256))
+    # ensure password hash field has length of at least 256
+    password_hash = db.Column(db.String(256), nullable=False)
     # Additional fields to demonstrate in vulnerable profile
     bio = db.Column(db.String(500))
     phone = db.Column(db.String(20))
